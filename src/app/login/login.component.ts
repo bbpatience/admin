4e7511793696dee901e6ab2ec754dd4fdcb402b7
baibing <baibing@rongcloud.cn>
2018-12-31 12:17:01 +0800

    add service to connect to server.  failed now

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from '../service/user.service';
+import { LoginInfo } from '../service/login.info';
 
 @Component({
     selector: 'app-login',
@@ -7,12 +9,26 @@ import { Router } from '@angular/router';
     styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-    constructor(private router: Router) {}
+
+    constructor(
+        private router: Router,
+        private service: UserService) {
+    }
 
     ngOnInit() {}
 
     onLogin() {
+        // TODO username password.
+        const info = new LoginInfo('username', 'fd');
+        console.log('hello');
         localStorage.setItem('isLoggedin', 'true');
         this.router.navigate(['/dashboard']);
+        // this.service.login(info).subscribe(result => {
+        //     console.log(result.code);
+        //     if (result.code === 10000) {
+        //         localStorage.setItem('isLoggedin', 'true');
+        //         this.router.navigate(['/dashboard']);
+        //     }
+        // });
     }
 }
